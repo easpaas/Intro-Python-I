29f79820c35c1e549624c3d1eff37bd40e3c92bb
evan <aspaasevan@gmail.com>
2020-05-31 19:14:28 -0700

    01, 02 complete

--- src/01_bignum.py
+++ src/01_bignum.py
@@ -2,3 +2,6 @@
 # (try doing the same thing in the JS console and see what it outputs)
 
 # YOUR CODE HERE
+powerOfX = 65536
+
+print(2**powerOfX) ## Very large number, JS console returns 'Infinity'
